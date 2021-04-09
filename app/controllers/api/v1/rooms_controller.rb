17eeb60c87c55afa787797c5c85268decf91c05f
Cassandra <cparisi1290@gmail.com>
2021-04-09 12:37:03 -0400

    include plants association

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::RoomsController < ApplicationController
   
   def index
     rooms = Room.all
+    options = {include: [:plants]}
     render json: RoomSerializer.new(rooms)
   end
 end
--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -1,4 +1,5 @@
 class RoomSerializer
   include FastJsonapi::ObjectSerializer
+  has_many :plants
   attributes :name
 end
