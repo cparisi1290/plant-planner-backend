56fb20675cb2512f49b08012060c33c6449291fa
Cassandra <cparisi1290@gmail.com>
2021-04-09 12:01:27 -0400

    update index action with plant serializer, add attributes to plant serializer

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,4 +1,4 @@
 class PlantSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :name, :watering_day, :light, :water, :food, :humidity, :toxicity, :additional_care, :image_url, :temperature, :room
 end
