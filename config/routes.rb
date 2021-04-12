d71ac2e6614795a32c3e05562f295a6878dc2952
Cassandra <cparisi1290@gmail.com>
2021-04-12 14:28:49 -0400

    include destroy in plant routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'rooms/name'
   namespace :api do
     namespace :v1 do
-      resources :plants, only: [:index, :create]
+      resources :plants, only: [:index, :create, :destroy]
       resources :rooms, only: [:index]
     end
   end
